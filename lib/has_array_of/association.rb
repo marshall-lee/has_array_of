879b87bd82a3c07d27fb77075258aec360829316
Vladimir Kochnev <v.kochnev@1tvch.ru>
2014-09-23 20:58:46 +0400

    implement of association scope

--- lib/has_array_of/association.rb
+++ lib/has_array_of/association.rb
@@ -5,7 +5,26 @@ module HasArrayOf
     end
 
     module ClassMethods
-      def has_array_of
+      def has_array_of(name, scope = nil, options = {}, &extension)
+        if scope.is_a?(Hash)
+          options = scope
+          scope = nil
+        end
+        if block_given?
+          mod = Module.new(&Proc.new)
+          if scope
+            proc { |owner| instance_exec(owner, &scope).extending(mod) }
+          else
+            proc { extending(mod) }
+          end
+        end
+        singular_name = name.to_s.singularize
+        class_name = (options[:class_name] || singular_name.camelize).to_s
+        ids_name = "#{singular_name}_ids".to_sym
+        model = class_name.constantize
+        define_method name do
+          model.where(model.arel_table[model.primary_key].in(send(ids_name)))
+        end
       end
     end
   end
--- spec/lib/has_array_of/association_spec.rb
+++ spec/lib/has_array_of/association_spec.rb
@@ -5,4 +5,44 @@ RSpec.describe HasArrayOf::Association do
     subject { described_class }
     it { should respond_to(:has_array_of) }
   end
+
+  describe "association scope" do
+    with_model :Video do
+      table do |t|
+        t.text :title
+      end
+    end
+    with_model :Playlist do
+      table do |t|
+        t.integer :video_ids, array: true, default: []
+      end
+
+      model do
+        has_array_of :videos
+      end
+    end
+
+    before do
+      # create another videos
+      Video.create(title: "my little pony season 01 episode 01")
+      Video.create(title: "my little pony season 01 episode 02")
+    end
+    let(:playlist_videos) {
+      [ Video.create(title: "crazy show about unicorns episode 1"),
+        Video.create(title: "crazy show about unicorns episode 2") ]
+    }
+
+    let!(:playlist) {
+      Playlist.create(video_ids: playlist_videos.map(&:id))
+    }
+
+    it "should respond to scope method" do
+      expect(playlist).to respond_to(:videos)
+    end
+
+    it "should fetch correct videos" do
+      expect(Video.count).to eq(4)
+      expect(playlist.videos).to contain_exactly(*playlist_videos)
+    end
+  end
 end
